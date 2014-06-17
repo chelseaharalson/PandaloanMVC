0aa4ade7e9359c24d759aa4b056c58e5a15bbd12
Chelsea Metcalf <chelseametcalf@ufl.edu>
2014-06-16 22:04:32 -0500

    Created database and home page

---
+++ Pandaloan/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PandaLoan.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public DateTime lastlogin { get; set; }
+    }
+
+    public class PandaloanDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
