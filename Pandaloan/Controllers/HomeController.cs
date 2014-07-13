16bf6740de956c119a5ee5b47935910b4171dd5f
Chelsea Metcalf <chelseametcalf@ufl.edu>
2014-07-13 17:44:55 -0500

    Added about and contact page.

--- Pandaloan/Controllers/HomeController.cs
+++ Pandaloan/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Pandaloan.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About Pandaloan";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Pandaloan";
 
             return View();
         }
