6e081ca6e27a58c5ee81fbfc23892a238b76e825
Chelsea Metcalf <chelseametcalf@ufl.edu>
2014-07-17 21:46:37 -0500

    Created the loan object.

---
+++ Pandaloan/Models/Loan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pandaloan.Models
+{
+    public class Loan
+    {
+        public int loanID { get; set; }
+        public int borrowerID { get; set; }
+        public int lenderID { get; set; }
+        public double loanAmount { get; set; }
+        public DateTime loanDate { get; set; }
+        public int loanPeriod { get; set; }
+        public double interestRate { get; set; }
+        public int creditScore { get; set; }
+        public string loanComment { get; set; }
+    }
+}
